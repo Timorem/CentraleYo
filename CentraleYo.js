06968afa2af51beecdbaf3e4efb5723603deec71
Achraf Gharbi <achraf.gharbi@student.ecp.fr>
2016-02-11 16:19:07 +0100

    Rajout de la page adminbar

--- CentraleYo.js
+++ CentraleYo.js
@@ -1,10 +1,13 @@
 Compteur = new Mongo.Collection("compteur"); //servira à contenir le nb de clics
+Plat = new Mongo.Collection("plat"); //servira à contenir les plats
+Pizza = new Mongo.Collection("pizza");
 
 Router.route('home', {path:'/'});
 Router.route('admin', {path:'/adminyo'});
+Router.route('adminbar', {path:'/adminbar'});
 
 Router.configure({
-	loadingTemlate:'loading'
+	loadingTemplate:'loading'
 })
 
 var isButtonEnabled = function()
@@ -46,7 +49,12 @@ Meteor.methods({
             appId:'44_4a0dp01spse804ok4kwc44ccc0w8000sc0ocog8w8cc48040c0',
             secret:"4v0puc9sdh8gs4k8wsscw4gkggw0ocwks84os4s8kgkoosw4wo"
         });
-
+    },
+    checkPlat: function(platId, setChecked){
+    	Plat.update(platId, { $set :{dispo: setChecked} });
+    },
+    checkPizza: function(pizzaId, setChecked){
+    	Pizza.update(pizzaId, { $set :{dispo: setChecked} });
     }
 });
 
@@ -64,13 +72,49 @@ if(Meteor.isServer) {
 		Compteur.insert({compt:0, resetDate: Date()});
 	}
 
+	if (!Plat.findOne()){
+		Plat.insert({name:"Tartiflette", dispo:false});
+		Plat.insert({name:"Poulet Curry", dispo:false});
+		Plat.insert({name:"Gnocci chevre epinard", dispo:false});
+		Plat.insert({name:"Poulet pates champi", dispo:false});
+		Plat.insert({name:"Risotto", dispo:false});
+		Plat.insert({name:"Lasagne", dispo:false});
+		Plat.insert({name:"Poulet basquaise", dispo:false});
+		Plat.insert({name:"Chili con carne", dispo:false});
+		Plat.insert({name:"Tajine de poulet", dispo:false});
+		Plat.insert({name:"Poulet teriyaki", dispo:false});
+		Plat.insert({name:"Raviolis ricotta épinard", dispo:false});
+		Plat.insert({name:"Poulet sauce citron purée de patates douces", dispo:false});
+	}
+
+	if (!Pizza.findOne()) {
+		Pizza.insert({name:"Kebab", dispo:false});
+		Pizza.insert({name:"Royale", dispo:false});
+		Pizza.insert({name:"Saumon", dispo:false});
+		Pizza.insert({name:"Tomate mozzarella", dispo:false});
+		Pizza.insert({name:"Boeuf chorizo", dispo:false});
+		Pizza.insert({name:"4 Fromages", dispo:false});
+		Pizza.insert({name:"Tartiflette", dispo:false});
+		Pizza.insert({name:"Jambon speck", dispo:false});
+		Pizza.insert({name:"Campagnarde", dispo:false});
+		Pizza.insert({name:"Poulet moutarde", dispo:false});
+		Pizza.insert({name:"Chorizo", dispo:false});
+		Pizza.insert({name:"Chévre", dispo:false});
+		Pizza.insert({name:"Chévre miel", dispo:false});
+	}
+
 	Meteor.publish("compteur", function() {
 		return Compteur.find({});
 	});
     Meteor.publish("userData", function () {
         return Meteor.users.find({_id: this.userId});
     });
-
+  	Meteor.publish("plat", function() {
+		return Plat.find({});
+	});
+	Meteor.publish("pizza", function() {
+		return Pizza.find({});
+	});
 
     Meteor.call("config");
 }
@@ -79,7 +123,9 @@ if (Meteor.isClient) {
 	Push.enabled(true);
 	Meteor.subscribe("compteur");
     Meteor.subscribe("userData");
-
+	Meteor.subscribe("plat");
+    Meteor.subscribe("pizza");
+	
 	Template.registerHelper('counter', function () {
 			return Compteur.findOne().compt;
 		});
@@ -119,6 +165,12 @@ if (Meteor.isClient) {
         userName : function(){
             return Meteor.user().services.myecp.first_name;
         },
+        affichePlat : function(){
+        	return Plat.find({dispo: true});
+        },
+        affichePizza : function(){
+        	return Pizza.find({dispo: true});
+        },
 		isButtonActivated : function() {return isButtonEnabled();}
 	});
 
@@ -152,6 +204,41 @@ if (Meteor.isClient) {
 		"submit .adminform":function(event){
 			event.preventDefault();
 			Meteor.call("serverNotification", event.target.textarea.value, "Centrale YO");
+		}
+	});
+	
+	Template.Adminbar.helpers({
+		isConnected :function(){
+            return Meteor.userId();
+        },
+		listPizza: function(event){
+			return Pizza.find({});
+		},
+		listPlat: function(event){
+			return Plat.find({});
+		}
+	});
+
+	Template.Adminbar.events({
+		"submit .adminform":function(event){
+			event.preventDefault();
+			Meteor.call("serverNotification", event.target.textarea.value, "Centrale YO");	
+		},
+		"click .pizza-check":function(event){
+			event.preventDefault();
+			Meteor.call("checkPizza", this._id, ! this.checked);
+		},
+		"click .plat-check":function(event){
+			event.preventDefault();
+			Meteor.call("checkPlat", this._id, ! this.checked);
+		},
+		"click #myecp-login": function(event, template){
+			Meteor.loginWithMyECP({loginStyle: "redirect"}, function(err){
+				if (err) {
+					throw new Meteor.Error(err);
+				} else {
+					throw new Meteor.Error("LOL");
+				}});
 		}
 	});
 }
